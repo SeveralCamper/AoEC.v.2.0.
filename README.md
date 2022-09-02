f0ab59f7a36ffa6738cff86d099266854ae92d35
Several_notebook <mikha.alkhimovich@mail.ru>
2022-09-02 23:59:27 +0700

    develop: finish code structure

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # AoEC.v.2.0.
+
+Computing performance evaluation
---
+++ src/1/CP_evaluation.h
@@ -0,0 +1,11 @@
+#ifndef SRC_S21_MATH_H_
+#define SRC_S21_MATH_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+
+//  Computing performance evaluation
+
+
+
+#endif  //  SRC_S21_MATH_H_
