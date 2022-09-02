90b7bbf09d03ad0e50a5ab57b350f04dd5c8e4e0
Several_notebook <mikha.alkhimovich@mail.ru>
2022-09-03 02:02:15 +0700

    develop: have some problems

--- src/1/CP_evaluation.h
+++ src/1/CP_evaluation.h
@@ -1,11 +1,30 @@
-#ifndef SRC_S21_MATH_H_
-#define SRC_S21_MATH_H_
+#ifndef SRC_CP_EVALUTION_H_
+#define SRC_CP_EVALUTION_H_
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/time.h>
+#include <time.h>
 
 //  Computing performance evaluation
 
+//  gettimeofday()
+struct timeval time_val_1, time_val_2, data_time_val;
 
+void time_start();
+void first_CPE_check();
 
-#endif  //  SRC_S21_MATH_H_
+double time_stop();
+
+//  times()
+
+struct tms tmsBegin, tmsEnd;
+
+void time_start_2();
+void second_CPE_check();
+
+long time_stop_2();
+
+clock_t times(struct tms *buf);
+
+#endif  //  SRC_CP_EVALUTION_H_
